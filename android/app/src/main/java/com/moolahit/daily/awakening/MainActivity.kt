48330e30c00ca199718061611e44f1f9ba237f9c
Veronika <veronikapetrushka@gmail.com>
2025-05-23 09:35:42 +0200

    android

--- android/app/src/main/java/com/moolahit/daily/awakening/MainActivity.kt
+++ android/app/src/main/java/com/moolahit/daily/awakening/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.moolahitdailyawakening
+package com.moolahit.daily.awakening
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/moolahit/daily/awakening/MainApplication.kt
+++ android/app/src/main/java/com/moolahit/daily/awakening/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.moolahitdailyawakening
+package com.moolahit.daily.awakening
 
 import android.app.Application
 import com.facebook.react.PackageList
